2fa2e58e5ff15713b3d9f9ebd3094ca7b18d1c5f
Marco <marco_trabajo@yahoo.com>
2021-11-20 15:24:22 +0100

    Created useState() on App, passed the props to Topbar and changed state onClick using hamburger menu open/close

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,29 @@
 import './topbar.scss'
+import PersonIcon from '@mui/icons-material/Person';
+import MailIcon from '@mui/icons-material/Mail';
 
-export default function Topbar() {
+export default function Topbar( {menuOpen, setMenuOpen}) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")} >
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" class="logo"> genius. </a>
+          <a href="#intro" className="logo"> genius. </a>
+          <div className="itemContainer">
+            <PersonIcon className="icon"/>
+            <span>+34 697 26 08 31</span>
+          </div>
+          <div className="itemContainer">
+            <MailIcon className="icon"/>
+            <span>+34 697 26 08 31</span>
+          </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="hamburger" onClick={ () => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
       
     </div>
